867c3026fe37075bc2428111e8adcbfeff711234
marouan <marouanebellari@icloud.com>
2024-11-07 22:11:11 +0100

    parallelised 1.2 w OpenMP

--- lab1/assign_1_2_framework/simulate.c
+++ lab1/assign_1_2_framework/simulate.c
@@ -1,33 +1,71 @@
-/*
- * simulate.c
- *
- * Implement your (parallel) simulation here!
- */
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <omp.h>
 #include "simulate.h"
 
+#define C 0.15
 
-/*
- * Executes the entire simulation.
- *
- * Implement your code here.
- *
- * i_max: how many data points are on a single wave
- * t_max: how many iterations the simulation should run
- * num_threads: how many threads to use
- * old_array: array of size i_max filled with data for t-1
- * current_array: array of size i_max filled with data for t
- * next_array: array of size i_max. You should fill this with t+1
- */
+/* Global variables */
+double *old_array_global;
+double *current_array_global;
+double *next_array_global;
+int i_max_global;
+int t_max_global;
+int num_threads_global;
+
 double *simulate(const int i_max, const int t_max, const int num_threads,
-        double *old_array, double *current_array, double *next_array)
+                 double *old_array, double *current_array, double *next_array)
 {
-    /*
-     * Your implementation should go here.
-     */
+    old_array_global = old_array;
+    current_array_global = current_array;
+    next_array_global = next_array;
+    i_max_global = i_max;
+    t_max_global = t_max;
+    num_threads_global = num_threads;
 
-    return current_array;
+    int N = i_max - 2;
+    int chunk_size = N / num_threads;
+    int remainder = N % num_threads;
+    int i_start, i_end;
+
+    omp_set_num_threads(num_threads);
+
+    #pragma omp parallel
+    {
+        if (omp_get_thread_num() < remainder) {
+            i_start = 1 + omp_get_thread_num() * (chunk_size + 1);
+            i_end = i_start + chunk_size + 1;
+        } else {
+            i_start = 1 + omp_get_thread_num() * chunk_size + remainder;
+            i_end = i_start + chunk_size;
+        }
+
+        for (int t = 0; t < t_max; t++) {
+            for (int i = i_start; i < i_end; i++) {
+                next_array[i] = 2 * current_array[i] - old_array[i] +
+                                C * (current_array[i - 1] -
+                                (2 * current_array[i] - current_array[i + 1]));
+            }
+
+            #pragma omp barrier
+
+            #pragma omp single
+            {
+                double *temp = old_array_global;
+                old_array_global = current_array_global;
+                current_array_global = next_array_global;
+                next_array_global = temp;
+            }
+
+            #pragma omp barrier
+
+            old_array = old_array_global;
+            current_array = current_array_global;
+            next_array = next_array_global;
+        }
+    }
+
+    return current_array_global;
 }
+
